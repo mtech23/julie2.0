979ba0edd3b8d927cd28254e74d1ba969c761acb
mtech23 <frontendmarcell@gmail.com>
2024-05-07 12:36:06 +0500

    update

--- inc/header.php
+++ inc/header.php
@@ -15,7 +15,7 @@
 
     <header class="main_header">
         <nav class="navbar navbar-expand-lg navbar-light align-items-center ">
-            <a class="navbar-brand logo-name" href="index.php">LOGO HERE</a>
+            <a class="navbar-brand logo-name" href="index">LOGO HERE</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
